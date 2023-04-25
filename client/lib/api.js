462b272fee9516b0d3d2263878ed32d9368aa457
Namatullah <namatullah.shahbazi444@gmail.com>
2023-04-25 07:45:52 +0430

    api changes

--- client/lib/api.js
+++ client/lib/api.js
@@ -1,13 +1,5 @@
 import axios from 'axios';
 
-// const API = axios.create({
-//     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
-//     // headers: {
-//     //     'X-Requested-With': 'XMLHttpRequest',
-//     // },
-//     // withCredentials: true,
-// });
-
 const API = axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
     // headers: {
